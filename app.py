7f774be17f2fa1731e756a05ab92c6b15bb413e6
Nathan Dai <nathandai2000@gmail.com>
2021-09-07 19:12:41 -0700

    variable form submission

--- app.py
+++ app.py
@@ -7,17 +7,22 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if (request.method == 'POST'):
-		# if ('path' in request.args):
-		path = request.form['path']
+		if ('path' in request.form):
+			path = request.form['path']
 
-		print(f'Reading data from {path}')
-		try:
-			df = pd.read_csv(path, low_memory=False)
-		except FileNotFoundError:
-			print('File not found')
-			return render_template('index.html', error='File not found')
+			print(f'Reading data from {path}')
+			try:
+				df = pd.read_csv(path, low_memory=False)
+			except FileNotFoundError:
+				print('File not found')
+				return render_template('index.html', error='File not found')
 
-		return render_template('index.html', variables=df.columns, path=path)
+			return render_template('index.html', variables=df.columns, path=path)
+
+		else:
+			variables = list(request.form.keys())
+
+			return 'Hello World'
 
 	else: # request.method == 'GET'
 		return render_template('index.html')
--- static/javascript/app.js
+++ static/javascript/app.js
@@ -2,18 +2,21 @@ function add(variable) {
 	var element;
 
 	console.log(variable.children[0]);
-	if (variable.classList.contains('unused')) {
+	if (variable.classList.contains('unused')) { // switching to used
 		variable.classList.remove('unused');
 		variable.classList.add('used');
-		variable.children[0].innerText = 'Remove';
+		variable.children[1].innerText = 'Remove';
+
+		variable_input = variable.children[0];
+		variable_input.setAttribute('name', variable_input.value);
 
 		element = variable.cloneNode(true);
 		variable.remove();
 		document.getElementById('used-variables-list').appendChild(element);
-	} else if (variable.classList.contains('used')) {
+	} else if (variable.classList.contains('used')) { // switching to unused
 		variable.classList.remove('used');
 		variable.classList.add('unused');
-		variable.children[0].innerText = 'Add'
+		variable.children[1].innerText = 'Add'
 
 		element = variable.cloneNode(true);
 
